dcae20a2a0dcc720ab4f180737a61dcf72c140e5
Iqra Shaikh <Iqra.Shaikh@lntinfotech.com>
2021-03-23 17:54:57 +0530

    modify line 3

--- my-summy-proj/src/Hello.java
+++ my-summy-proj/src/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 	public void method1() {
 		System.out.println("method1");
 		System.out.println("method1 method method");
-		System.out.println("method1");
+		System.out.println("method1 method");
 	}
 }
