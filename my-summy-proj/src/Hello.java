fd2b79866d585ebd8157711b115a267938b5dfa6
Iqra Shaikh <Iqra.Shaikh@lntinfotech.com>
2021-03-23 17:52:40 +0530

    chnges

--- my-summy-proj/src/Hello.java
+++ my-summy-proj/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public void method1() {
 		System.out.println("method1");
-		System.out.println("method1");
+		System.out.println("method1 method method");
 		System.out.println("method1");
 	}
 }
